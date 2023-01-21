9a3ca2a7d22db06513ef0b2e91b32cf9b06273dd
suryatejasunkoju <sunkojusuryateja@gmail.com>
2023-01-21 21:20:21 +0530

    This is the solution for pizza billing

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,13 +2,29 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    protected int price;
     private Boolean isVeg;
     private String bill;
-
+    private int bagPrice=20;
+    private int toppingsPrice;
+    private int cheesePrice=80;
+    private boolean isExtraCheeseAdded;
+    private boolean isExtraToppingsAdded;
+    private boolean hasTakenAway;
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg){
+            price=300;
+            toppingsPrice=70;
+        }
+        else {
+            price = 400;
+            toppingsPrice = 120;
+        }
+        bill="Base Price Of The Pizza: "+price+"\n";
+        hasTakenAway=false;
+        isExtraCheeseAdded=false;
+        isExtraToppingsAdded=false;
     }
 
     public int getPrice(){
@@ -17,18 +33,41 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if (isExtraCheeseAdded){
+            //we  dont need to add again
+        }
+        else{
+            price+=cheesePrice;
+            bill+="Extra Cheese Added: "+cheesePrice+"\n";
+            isExtraCheeseAdded=true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (isExtraToppingsAdded){
+            //we  dont need to add again
+        }
+        else{
+            price+=toppingsPrice;
+            bill+="Extra Toppings Added: "+toppingsPrice+"\n";
+            isExtraToppingsAdded=true;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if (hasTakenAway){
+            //if already taken away no need to do anything
+        }
+        else {
+            this.price+=bagPrice;
+            bill+="Paperbag Added: "+bagPrice+"\n";
+            hasTakenAway=true;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        return this.bill+"Total Price: "+price+"\n";
     }
 }
